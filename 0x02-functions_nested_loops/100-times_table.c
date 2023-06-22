c31cc474b66c06616de390010eebee5500b95c55
JoyceAudrey <jaudrey.joyce@gmail.com>
2023-06-21 17:18:53 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,43 +2,39 @@
 
 /**
  * print_times_table - prints the n times table starting with 0
+ *
  * @n: number of times table
  */
 void print_times_table(int n)
 {
-	int x, y, z;
+	int num, mult, prod;
 
-	if  (n >= 0 && n <= 15)
+	if (n >= 0 && n <= 15)
 	{
-		for (x = 0; x <= n; x++)
-		{
-			for (y = 0; y <= n; y++)
-			{
-				z = x * y;
-				if (y == 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(z + '0');
-				} else if (z >= 10 && z < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((z / 10) + '0');
-					_putchar((z % 10) + '0');
-				} else if (z >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((z / 100) + '0');
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
-				}
-			}
-			_putchar('\n');
-		}
+		for (num = 0; num <= n; num++)
+	{
+		_putchar('0');
+	for (mult = 1; mult <= n; mult++)
+	{
+		_putchar(',');
+		_putchar(' ');
+		prod = num * mult;
+		if (prod <= 99)
+		_putchar(' ');
+		if (prod <= 9)
+		_putchar(' ');
+		if (prod >= 100)
+	{
+		_putchar((prod / 100) + '0');
+		_putchar(((prod / 10)) % 10 + '0');
+	}
+	else if (prod <= 99 && prod >= 10)
+	{
+		_putchar((prod / 10) + '0');
+	}
+		_putchar((prod % 10) + '0');
+	}
+		_putchar('\n');
+	}
 	}
 }
