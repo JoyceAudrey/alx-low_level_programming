3cd685ef7d34a5737d58f0156d6a401eb71d0122
JoyceAudrey <jaudrey.joyce@gmail.com>
2023-06-27 06:59:10 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,12 +7,12 @@
  *
  * Return: length of string
  */
-int _strlen(char *s)
+size_t _strlen(const char *str)
 {
-	int  string length = 0;
+	size_t length = 0;
 
-	while (s[string_length])
-		string_lenght++;
+	while (*str++)
+		length++;
 
-	return (string length);
+	return (length);
 }
--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,12 +3,15 @@
 /**
  *  _puts - function prints a string followed by a new line to stdout
  *  @str: string to print
+ *  Return: void
  */
+
 void _puts(char *str)
+
 {
-	while (*str != '\0')
+	for (; *str != '\0'; str++)
 	{
-		_putchar(*str++);
-		_putchar('\n');
+	_putchar(*str);
 	}
+	_putchar('\n');
 }
